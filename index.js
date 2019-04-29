dee34fd4dce3cd68fa71aeddc70fd9dff2d2dbad
fkrishna <fkrishna.dev@gmail.com>
2019-04-29 09:34:25 -0700

    Added new dependency

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ import { App } from './js/App'
 
 let $form = document.querySelector('#form');
 let $provider = document.querySelector('#provider');
+let $notif = document.querySelector('#notif');
 
 $form.addEventListener('submit', function(){
   let cardNumber = document.querySelector('#card-number').value;
   let card = App.getProvider(cardNumber);
   $provider.textContent = (card.provider) ? card.provider.name : 'Invalid';
+  
   log(card);
 });
