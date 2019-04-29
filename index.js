3eb4734c24d0c20216dcc70070df520057230e7c
fkrishna <fkrishna.dev@gmail.com>
2019-04-28 20:06:54 -0700

    driver code implementation

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-// Import stylesheets
-import './style.css';
+'use strict';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+import { log } from './js/utils';
+import { Card } from './js/Card';
+import { Provider } from './js/Provider';
+
+let $form = document.querySelector('#form');
+
+const PROVIDERS = [
+  new Provider('American Express', ['34', '37'], ['15']),
+  new Provider('Discover', ['6011', '622126-622925', '644-649', '65'], ['16']),
+  new Provider('MasterCard', ['2221-2720', '51-55'], ['16']),
+  new Provider('Visa', ['4'], ['13', '16', '19'])
+];
+
+
+$form.addEventListener('submit', function(){
+  let cardNumber = document.querySelector('#card-number').value;
+  let card = new Card(cardNumber);
+  card.provider = getProvider(card, PROVIDERS);
+  log(card);
+});
+
+function getProvider(card, PROVIDERS) {
+  for(let provider of PROVIDERS)
+    if(provider.matched(card))
+      return provider;
+  return null;
+}
